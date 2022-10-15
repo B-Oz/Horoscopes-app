7db4ec30c904a1a28077e52a7fc5a20aa353000b
B-Oz <bettysoftwareng@gmail.com>
2022-10-15 18:55:39 +0300

    useState

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
+import "./Card.css";
 
 const Card = ({ title, date, quartet, element, birthstones, desc, image }) => {
   const [Showcard, setShowcard] = useState(true);
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={image} alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+    <main>
+      <div onClick={() => setShowcard(!Showcard)}>
+        {Showcard ? (
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={image} alt="..." />
+          </div>
+        ) : (
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">{date}</li>
+                <li className="list-group-item">Characteristic : {quartet}</li>
+                <li className="list-group-item">Zodiac Element : {element}</li>
+                <li className="list-group-item">Birthstone: {birthstones}</li>
+              </ul>
+              <p className="card-text">{desc}</p>
+            </div>
+          </div>
+        )}
       </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-      </ul>
-      <div className="card-body">
-        <a
-          href="https://onedio.com/haber/burclarin-ogrenince-cok-sasiracaginiz-daha-once-hic-duyulmamis-ozellikleri-950002"
-          className="card-link"
-        >
-          Learn More
-        </a>
-      </div>
-    </div>
+    </main>
   );
 };
 
