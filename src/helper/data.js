67275970f37269e9df05e52d38d813c89f188f54
B-Oz <bettysoftwareng@gmail.com>
2022-10-15 13:19:45 +0300

    data.js

--- src/helper/data.js
+++ src/helper/data.js
@@ -1,4 +1,17 @@
-const data = [
+import aries from "../assets/horoscope-aries.jpg";
+import taurus from "../assets/horoscope-taurus.jpg";
+import gemini from "../assets/horoscope-gemini.jpg";
+import cancer from "../assets/horoscope-cancer.jpg";
+import leo from "../assets/horoscope-leo.jpg";
+import virgo from "../assets/horoscope-virgo.jpg";
+import libra from "../assets/horoscope-libra.jpg";
+import scorpio from "../assets/horoscope-scorpio.jpg";
+import sagittarius from "../assets/horoscope-sagittarius.jpg";
+import capricorn from "../assets/horoscope-capricorn.jpg";
+import aquarius from "../assets/horoscope-aquarius.jpg";
+import pisces from "../assets/horoscope-pisces.jpg";
+
+export const data = [
   {
     id: 1,
     title: "ARIES",
@@ -7,7 +20,7 @@ const data = [
     element: "fire",
     birthstones: "aquamarine, jasper , diamond",
     desc: "The first sign of astrology is Aries. People with Aries are active, energetic, free and courageous people. They like to manipulate people. People under the sign of Aries are enthusiastic and goal-oriented. In addition to having leadership qualities, they are courageous and assertive, and due to these characteristics, they can easily guide other people.",
-    image: "./img/horoscope-aries.jpg",
+    image: aries,
   },
   {
     id: 2,
@@ -17,7 +30,7 @@ const data = [
     element: "earth",
     birthstones: "emerald, agate, turquoise",
     desc: "Taurus is a reliable and stubborn zodiac sign. They are people who have a large group of friends with their warm personalities. They are generally described as art lovers. It has been observed that they often act with their emotions. Although Taurus people are self-indulgent and like to be lazy, with the contribution of their stubborn personalities, they can be determined about the things they want most and thus achieve success.",
-    image: "./img/horoscope-taurus.jpg",
+    image: taurus,
   },
   {
     id: 3,
@@ -27,7 +40,7 @@ const data = [
     element: "air",
     birthstones: "alexandrite, jade, tiger eye",
     desc: "Gemini people are known for experiencing multiple emotions at the same time. People belonging to the Gemini zodiac sign may not keep one memory of the other, so they are expressed as unbalanced by many people. Gemini people like to socialize and talk, so they are very popular in their environment. Gemini people have high self-confidence and are defined as an attractive personality in social environments.",
-    image: "./img/horoscope-gemini.jpg",
+    image: gemini,
   },
   {
     id: 4,
@@ -37,7 +50,7 @@ const data = [
     element: "water",
     birthstones: "ruby, amethyst, chalcedony",
     desc: "People belonging to the Cancer sign are known as sensitive and emotional people by their environment. Because of their sensitivity, Cancer people can be easily offended and hurt very quickly. As they fill their families, they become thinkers. They are sensitive and caring not only to their families but to every person they meet, so the emotional bonds they form with the people around them are very strong. They are known as responsible people in their social and business lives.",
-    image: "./img/horoscope-cancer.jpg",
+    image: cancer,
   },
   {
     id: 5,
@@ -47,7 +60,7 @@ const data = [
     element: "fire",
     birthstones: "agate, amethyst, amber",
     desc: "People of the Leo sign are authoritarian and like to manage the people around them in both social and business life. Leos are also generous and self-sacrificing people. When the people around them are in trouble, they do everything they can to help them, so they are loved and respected around them.",
-    image: "./img/horoscope-leo.jpg",
+    image: leo,
   },
   {
     id: 6,
@@ -57,7 +70,7 @@ const data = [
     element: "earth",
     birthstones: "emerald, sapphire, topaz",
     desc: "Virgo people are very organized and meticulous in their work. They do not make sudden decisions, whatever they are doing, they first analyze and decide by thinking. They care about cleanliness and order. This is one of their strongest traits as they approach everything logically, and they can find practical and rational solutions to problems.",
-    image: "./img/horoscope-virgo.jpg",
+    image: virgo,
   },
   {
     id: 7,
@@ -67,7 +80,7 @@ const data = [
     element: "air",
     birthstones: "jade, pearl, topaz",
     desc: "Libra, people of the zodiac sign are recognized diplomatically in their environment and come to the fore with these features. Because Libras are diplomatic, they can establish good relations with almost everyone, including people with opposing views. He is ambitious in business life, has high ambitions for success and making money. They do not hesitate to spend their earnings on expensive pleasures and goods. They exhibit a very attractive appearance in their environment.",
-    image: "./img/horoscope-libra.jpg",
+    image: libra,
   },
   {
     id: 8,
@@ -77,7 +90,7 @@ const data = [
     element: "water",
     birthstones: "aquamarine, sapphire, black pearl",
     desc: "Scorpio people are known for their passionate lifestyles. In social life, with their friendly personalities and attractive images, they easily enter any environment and make themselves loved. They prioritize the mind and love to question. For Scorpio people, revenge is like a dish best served cold. If a wrong has been done against them, they never forget it and when the time comes, they take action to take revenge.",
-    image: "./img/horoscope-scorpio.jpg",
+    image: scorpio,
   },
   {
     id: 9,
@@ -87,30 +100,36 @@ const data = [
     element: "fire",
     birthstones: "amethyst, sapphire, turquoise",
     desc: "Sagittarius people have a positive and lively personality towards life. In social life, a Sagittarius is easy to meet new people and they like to have fun with their friends. They have positive thinking and good intentions, but this may cause them to make wrong decisions from time to time and to regret these decisions. ",
-    image: "./img/horoscope-sagittarius.jpg",
+    image: sagittarius,
   },
   {
     id: 10,
     title: "CAPRİCORN",
-    date: "22 October - 23 December",
-    quartet: "mutable",
-    element: "fire",
-    birthstones: "amethyst, sapphire, turquoise",
-    desc: "Sagittarius people have a positive and lively personality towards life. In social life, a Sagittarius is easy to meet new people and they like to have fun with their friends. They have positive thinking and good intentions, but this may cause them to make wrong decisions from time to time and to regret these decisions. ",
-    image: "./img/horoscope-sagittarius.jpg",
+    date: "22 December - 20 January",
+    quartet: "pioneer",
+    element: "earth",
+    birthstones: "garnet, agate, fluorite",
+    desc: "Capricorns are meticulous, ambitious and hardworking in their work and they want to have a good career. With their ambitious nature, they constantly plan for their career and future, but this sometimes makes them worry. Although they are described as cold at work, if you can be friends you can quickly realize that they are also quite funny people. They tend to be depressed when they don't get what they want or can't achieve their goals.",
+    image: capricorn,
   },
   {
     id: 11,
-    title: "Arashiyama Bamboo Grove",
-    desc: "Bamboo Forest, or Arashiyama Bamboo Grove or Sagano Bamboo Forest, is a natural forest of bamboo in Arashiyama, Kyoto, Japan. The forest consists mostly of Moso Bamboo (Phyllostachys edulis) and consists of several pathways for tourists and visitors. The Ministry of the Environment considers it a part of the soundscape of Japan.",
-    image: "https://wallpaperaccess.com/full/534182.jpg",
+    title: "AQUARIUS",
+    date: "21 January - 19 February",
+    quartet: "fixed",
+    element: "air",
+    birthstones: "aquamarine, turquoise, sapphire,",
+    desc: "Aquarius people are free-spirited people who like to do their own thing. They often don't care what people around them think of them. Since they can question the events well, they do not take sides and are neutral towards the events. Their impartiality also becomes one of their strongest traits, and they are seen by many as truthful and sincere.",
+    image: aquarius,
   },
   {
     id: 12,
-    title: "Antelope Canyon",
-    desc: "Antelope Canyon is a slot canyon in the American Southwest, on Navajo land east of Page, Arizona. It includes two separate, scenic slot canyon sections, referred to as Upper Antelope Canyon (or The Crack), and Lower Antelope Canyon (or The Corkscrew).",
-    image:
-      "https://images.unsplash.com/photo-1444076784383-69ff7bae1b0a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxvd2VyJTIwYW50ZWxvcGUlMjBjYW55b258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    title: "PİSCES",
+    date: "20 February - 20 March",
+    quartet: "mutable",
+    element: "water",
+    birthstones: "aquamarine, amethyst, sapphire",
+    desc: "Pisces people are very emotional people. Because they are emotional, they are compassionate and well-intentioned, and thus they have no difficulty in establishing very close friendships. Although this is their strongest side, it can also make them quite melancholy and pessimistic about things. They are very good friends and very good confidants. It could even be argued that there is no sign that is better at keeping secrets than them.",
+    image: pisces,
   },
 ];
-export default data;
